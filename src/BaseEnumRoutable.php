036cc602c05d9b2d141def57620f73d7190a60f2
Jérémy Munsch <jeremy.munsch@ldc.fr>
2021-08-13 12:11:43 +0200

    Base Enum routable bump to version 2.0.0

---
+++ src/BaseEnumRoutable.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Kwaadpepper\Enum;
+
+use BadMethodCallException;
+use Illuminate\Contracts\Routing\UrlRoutable;
+use Kwaadpepper\Enum\Exceptions\NotImplementedException;
+
+abstract class BaseEnumRoutable extends BaseEnum implements UrlRoutable
+{
+    /**
+     * Get the value of the model's route key.
+     *
+     * @return mixed
+     */
+    public function getRouteKey()
+    {
+        return $this->value;
+    }
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        throw new NotImplementedException('getRouteKeyName not implemented, should not be used');
+    }
+
+    /**
+     * Retrieve the model for a bound value.
+     *
+     * @param  mixed  $value
+     * @param  string|null  $field
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
+    public function resolveRouteBinding($value, $field = null)
+    {
+        // try to cast numeric value first
+        try {
+            return static::make(is_numeric($value) ? intval($value) : $value);
+        } catch (BadMethodCallException $e) {
+        }
+        // try string value after that
+        try {
+            return static::make($value);
+        } catch (BadMethodCallException $e) {
+        }
+        // could not find a suitable value
+        return null;
+    }
+
+    /**
+     * Retrieve the child model for a bound value.
+     *
+     * @param  string  $childType
+     * @param  mixed  $value
+     * @param  string|null  $field
+     * @return \Illuminate\Database\Eloquent\Model|null
+     * @throws NotImplementedException
+     */
+    public function resolveChildRouteBinding($childType, $value, $field)
+    {
+        throw new NotImplementedException('resolveChildRouteBinding not implemented, should not be used');
+    }
+
+    /**
+     * Get the Enum name from class name
+     *
+     * @return string
+     */
+    public static function getEnumName()
+    {
+        $cls = explode('\\', static::class);
+        return array_pop($cls);
+    }
+}
